989fb92e90b144ae5427a9fc6858fbfa8f53b798
Dave Newton <davelnewton@gmail.com>
2011-09-06 18:54:52 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleAjaxToggle::Application.config.secret_token = '884947f8cf04ed4efa2481fa7abfeeb685c18b5a551d599c20e71f4db35cbd7b2d0570232fb8b2872625ab39ffa521ad3382c9b768d34022c02ac074522a15f7'
